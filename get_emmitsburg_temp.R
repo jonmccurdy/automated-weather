45d9071bb46413b2e9efe0bce334140c2f000abf
jonmccurdy <79713544+jonmccurdy@users.noreply.github.com>
2025-11-03 20:57:05 -0500

    Update get_emmitsburg_temp.R

--- get_emmitsburg_temp.R
+++ get_emmitsburg_temp.R
@@ -10,7 +10,7 @@ data <- fromJSON(content(res, "text", encoding = "UTF-8"))
 temp_c <- data$properties$temperature$value
 temp_f <- temp_c * 9/5 + 32
 
-df <- data.frame(time = Sys.time(), station = "KHGR", temp_c = temp_c, temp_f = temp_f)
+df <- data.frame(time = format(Sys.time(), tz = "America/New_York", usetz = TRUE), station = "KHGR", temp_c = temp_c, temp_f = temp_f)
 
 dir.create("data", showWarnings = FALSE)
 file <- "data/emmitsburg_weather.csv"
@@ -19,4 +19,5 @@ if (file.exists(file)) {
   write_csv(df, file, append = TRUE)
 } else {
   write_csv(df, file)
+
 }
